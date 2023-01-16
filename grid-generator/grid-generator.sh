e7198264e5bfde7426d37ba2b37ef5c538747eee
DmitryMI <dimamon6032403@gmail.com>
2023-01-16 21:19:41 +0100

    netedit will open poly file automatically

--- grid-generator/grid-generator.sh
+++ grid-generator/grid-generator.sh
@@ -1,5 +1,8 @@
-netgenerate --grid --grid.number 5 --output-file generated-grid.net.xml
+proj_name="generated-grid"
+grid_size=5
 
-./grid-generator-vs/grid_generator_vs.py generated-grid 5 5
+netgenerate --grid --grid.number $grid_size --output-file $proj_name.net.xml
 
-netedit net.net.xml
+./grid-generator-vs/grid_generator_vs.py $proj_name $grid_size $grid_size
+
+netedit -s $proj_name.net.xml -a $proj_name.poly.xml
