de7f74deee5cdb579d52987bc08f1e127b9d1537
DmitryMI <dimamon6032403@gmail.com>
2023-01-19 19:40:11 +0100

    Routes generation implemented

--- grid-generator/grid-generator.sh
+++ grid-generator/grid-generator.sh
@@ -1,8 +1,30 @@
-proj_name="generated-grid"
-grid_size=5
+PROJ_NAME="generated-grid"
+GRID_SIZE=7
+SUMO_TOOLS_PATH="/home/dmitriy/software/sumo-1.8.0/tools"
+PROJ_DIR="./generated/"
 
-netgenerate --grid --grid.number $grid_size --output-file $proj_name.net.xml
+# Number of trips (vehicles) will be (TRIPS_END_TIME - TRIPS_START_TIME) / TRIPS_PERIOD
+TRIPS_START_TIME=0
+TRIPS_END_TIME=60
+TRIPS_PERIOD=1		# Must be positive!
 
-./grid-generator-vs/grid_generator_vs.py $proj_name $grid_size $grid_size
+# Number of intermediate points in a trip
+TRIPS_INTERMEDIATES=64
 
-netedit -s $proj_name.net.xml -a $proj_name.poly.xml
+mkdir $PROJ_DIR
+cd $PROJ_DIR
+
+# Generating a grid-network of roads
+netgenerate --grid --grid.number $GRID_SIZE --output-file $PROJ_NAME.net.xml
+
+# Generating buildings
+../grid-generator-vs/grid_generator_vs.py $PROJ_NAME $GRID_SIZE $GRID_SIZE
+
+# Generating random trips
+python3 $SUMO_TOOLS_PATH/randomTrips.py -n $PROJ_NAME.net.xml -b $TRIPS_START_TIME -e $TRIPS_END_TIME -p $TRIPS_PERIOD --random -i $TRIPS_INTERMEDIATES -o $PROJ_NAME.trips.xml
+
+# Generating actual vehicle routes based on trips
+duarouter -n $PROJ_NAME.net.xml --route-files $PROJ_NAME.trips.xml -o $PROJ_NAME.rou.xml
+
+# Visualize the road network and buildings
+# netedit -s $PROJ_NAME.net.xml -a $PROJ_NAME.poly.xml
