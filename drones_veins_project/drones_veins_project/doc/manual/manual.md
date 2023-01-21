d7648e19eca7fcd87a53ac2e52ac88f56c6172e2
DmitryMI <dimamon6032403@gmail.com>
2023-01-21 21:39:39 +0100

    Project generation script improved.

---
+++ drones_veins_project/drones_veins_project/doc/manual/manual.md
@@ -0,0 +1,7 @@
+# Drones Veins Project {#manual}
+
+[TOC]
+
+Welcome to the Drones Veins Project user manual.
+This page is the starting point for additional documentation of the Drones Veins Project project.
+
--- generate-veins-project.sh
+++ generate-veins-project.sh
@@ -14,12 +14,31 @@ USE_SIMU5G="1"
 # To remove a git repository created by cookiecutter
 REMOVE_GIT_REPO=1
 
+# To force project regeneration
+FORCE_REGENERATION=0
 
 ECHO_MSG="$PROJ_NAME\n$PROJ_BRIEF\n$PROJ_NAME_AS_FILE_NAME\n$PROJ_NAME_AS_MACRO_NAME"
 ECHO_MSG="$ECHO_MSG\n$USE_INET\n$USE_INET3\n$USE_VEINS_VLC\n$USE_PLEXE\n$USE_SIMULTE"
 ECHO_MSG="$ECHO_MSG\n$USE_SIMU5G\n"
 
-printf "$ECHO_MSG"
+while getopts ":hf" option; do
+	case $option in
+      		h) # display Help
+		 	echo "-f to force project regeneration"
+		 	echo "-h to show this help message"
+		 	exit;;
+     		f) # force regeneration
+     			echo "Force regeneration enabled"
+     			FORCE_REGENERATION=1     			
+	esac
+done
+
+if [[ $FORCE_REGENERATION == 1 ]];
+then
+	echo "Removing project files..."
+	rm -rf $PROJ_NAME_AS_FILE_NAME
+	echo "Project files removed"
+fi
 
 printf "$ECHO_MSG" | cookiecutter ./veins-project-generator
 
