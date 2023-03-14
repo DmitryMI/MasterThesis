e16e378e03a3fc10a2bccf84f561e27182f5b8f5
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-03-14 03:30:49 +0100

    Some changes

--- software-installation/_install_prereq.sh
+++ software-installation/_install_prereq.sh
@@ -21,8 +21,7 @@ if [[ $RUNTIME_ONLY == 0 ]]
 then
     $SUDO_PREFIX apt -y install git-core gitk git-gui tig clang bison flex tcl-dev tk-dev openjdk-17-jre qtbase5-dev libopenscenegraph-dev xvfb libwebkit2gtk-4.0-dev doxygen graphviz python3-numpy python3-pandas python3-matplotlib libxerces-c-dev libproj-dev libgdal-dev libfox-1.6-dev libavformat-dev libavcodec-dev libswscale-dev python-dev cmake xterm clang-format uncrustify valgrind gdb lldb nemiver
 else
-    echo "\nINSTALLING ONLY RUNTIME PREREQUISISTES!\n"
-    $SUDO_PREFIX apt -y install openjdk-17-jre python3-numpy python3-pandas python3-matplotlib libxerces-c-dev libproj-dev python-dev cmake clang-format uncrustify lldb
+    printf "\nINSTALLING ONLY RUNTIME PREREQUISISTES!\n"
+    $SUDO_PREFIX apt -y install git-core gitk git-gui tig clang bison flex tcl-dev tk-dev openjdk-17-jre qtbase5-dev libopenscenegraph-dev xvfb libwebkit2gtk-4.0-dev doxygen graphviz python3-numpy python3-pandas python3-matplotlib libxerces-c-dev libproj-dev libgdal-dev libfox-1.6-dev libavformat-dev libavcodec-dev libswscale-dev python-dev cmake xterm clang-format uncrustify valgrind gdb lldb nemiver
 fi
 
-$SUDO_PREFIX apt -y install bison flex
