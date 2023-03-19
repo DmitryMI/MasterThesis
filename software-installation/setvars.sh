537c670cd3d01c37c6f11e001e3404ad5a6f5619
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-03-19 01:58:03 +0100

    Fixed

--- software-installation/setvars.sh
+++ software-installation/setvars.sh
@@ -2,15 +2,20 @@
 
 INSTALLATION_DIR="/home/$(whoami)/Software"
 
-if [[ -z "$INSTALLATION_DIR_SUMO" ]]
+if [ -z "$INSTALLATION_DIR_SUMO" ]
 then
     INSTALLATION_DIR_SUMO="$INSTALLATION_DIR"
     echo "INSTALLATION_DIR_SUMO no set. Setting to default $INSTALLATION_DIR_SUMO"
+else
+    echo "SUMO will be installed to $INSTALLATION_DIR_SUMO"
 fi
-if [[ -z "$INSTALLATION_DIR_OMNETPP" ]]
+
+if [ -z "$INSTALLATION_DIR_OMNETPP" ]
 then
     INSTALLATION_DIR_OMNETPP="$INSTALLATION_DIR"
     echo "INSTALLATION_DIR_OMNETPP no set. Setting to default $INSTALLATION_DIR_OMNETPP"
+else
+    echo "OMNeT++ will be installed to $INSTALLATION_DIR_OMNETPP"
 fi
 
 DOWNLOADS_DIR="/home/$(whoami)/Downloads"
--- software-installation/setvars_runtime.sh
+++ software-installation/setvars_runtime.sh
@@ -6,13 +6,15 @@ if [ -z "$INSTALLATION_DIR_SUMO" ]
 then
     INSTALLATION_DIR_SUMO="$INSTALLATION_DIR"
     echo "INSTALLATION_DIR_SUMO no set. Setting to default $INSTALLATION_DIR_SUMO"
-echo
+else
     echo "SUMO will be installed to $INSTALLATION_DIR_SUMO"
 fi
+
 if [ -z "$INSTALLATION_DIR_OMNETPP" ]
 then
     INSTALLATION_DIR_OMNETPP="$INSTALLATION_DIR"
     echo "INSTALLATION_DIR_OMNETPP no set. Setting to default $INSTALLATION_DIR_OMNETPP"
+else
     echo "OMNeT++ will be installed to $INSTALLATION_DIR_OMNETPP"
 fi
 
