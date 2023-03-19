cc7662d375f650feaf0d860e34c9caee4d5ff110
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-03-19 01:53:07 +0100

    Fixed

--- software-installation/install_all.sh
+++ software-installation/install_all.sh
@@ -2,10 +2,10 @@
 
 if test -z "$1" 
 then
-      echo "Env file not specified"
+    echo "Env file not specified"
 else
-      echo "Sourcing $1"
-      source $1
+    echo "Sourcing $1"
+    source $1
 fi
 
 if [ $SETVARS_GUARD != 1 ]
@@ -14,41 +14,23 @@ then
     exit -1
 fi
 
-function run_installation_script () {
-	echo "Running $1..."
-	source $1
-	sleep $SLEEP_SEC 
-	
-	if [[ $? != 0 ]];
-	then
-		exit -1
-	fi
-	
-	printf "\n\n"
-}
+mkdir -p $INSTALLATION_DIR_SUMO
+mkdir -p $INSTALLATION_DIR_OMNETPP
 
-pwd
+. ./_install_prereq.sh
 
-if test -f "$INSTALLATION_DIR"; then
-	printf "Installation directory already exists."
-else
-	mkdir $INSTALLATION_DIR
-fi
+. ./_install_omnetpp.sh
 
-run_installation_script "./_install_prereq.sh"
+. ./_install_sumo.sh
 
-run_installation_script "./_install_omnetpp.sh"
+. ./_install_veins.sh
 
-run_installation_script "./_install_sumo.sh"
-
-run_installation_script "./_install_veins.sh"
-
-if [[ $RUNTIME_ONLY == 0 ]]
+if [ $RUNTIME_ONLY == 0 ]
 then
-    run_installation_script "./_install_python311.sh"
-    run_installation_script "./_install_r.sh"
-    run_installation_script "./_install_cookiecutter.sh"
-    run_installation_script "./_install_singularity.sh"
+    . ./_install_python311.sh
+    . ./_install_r.sh
+    . ./_install_cookiecutter.sh
+    . ./_install_singularity.sh
 fi
 
 printf "All done! Reboot required for changes in .profile to be applied.\n\n"
