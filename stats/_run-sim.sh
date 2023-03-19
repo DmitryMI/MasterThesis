cdb99a7ef905dbd2133bb678323a8686275cc75e
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-03-19 20:52:23 +0100

    Fixes

--- stats/_run-sim.sh
+++ stats/_run-sim.sh
@@ -2,16 +2,29 @@ cd $SIMULATION_DIR
 
 SUMO_PORT=9998
 
-echo "Starting veins_launchd from $VEINS_LAUNCHD_PATH..."
-$VEINS_LAUNCHD_PATH -kd --port $SUMO_PORT
-if [ $? != 0 ]; then exit $?; fi
+if [ -z "$SIM_AUTO_MANAGE_SUMO" ]
+then
+    SIM_AUTO_MANAGE_SUMO=0
+fi
+
+if [ $SIM_AUTO_MANAGE_SUMO != 0 ]
+then
+    echo "Starting veins_launchd from $VEINS_LAUNCHD_PATH..."
+    $VEINS_LAUNCHD_PATH -kd --port $SUMO_PORT
+    if [ $? != 0 ]; then exit $?; fi
+fi
 
 pwd
 opp_runall -b4 -j8 opp_run -m -u Cmdenv -c $OPP_CONFIG_NAME -n .:../../src/drones_veins_project:../../../veins/examples/veins:../../../veins/src/veins --image-path=../../images:../../../veins/images -l ../../src/drones_veins_project -l ../../../veins/src/veins omnetpp.ini --*.manager.port=$SUMO_PORT
 
 cd $WORKING_DIR
 
-pid=$(cat /tmp/sumo-launchd.pid)
-echo "Killing sumo running with pid $pid..."
+if [ $SIM_AUTO_MANAGE_SUMO != 0 ]
+then
 
-kill -9 $pid
+    pid=$(cat /tmp/sumo-launchd.pid)
+    echo "Killing sumo running with pid $pid..."
+
+    kill -9 $pid
+    
+fi
