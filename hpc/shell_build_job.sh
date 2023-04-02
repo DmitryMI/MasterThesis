43324d89b7615c134aecd35347d007fe0341e9e8
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-02 19:36:29 +0200

    Build job fixed

--- hpc/shell_build_job.sh
+++ hpc/shell_build_job.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Updating repository..."
 
-echo "source ~/setvars.sh && source ~/hpc-executors/clone_repo.sh && exit" | sshpass -f "$HPC_SSH_PASSWORD_FILE" ssh dmmo937c@taurus.hrsk.tu-dresden.de
+echo "source ~/scripts/hpc/setvars.sh && source ~/scripts/hpc/hpc-executors/clone_repo.sh && exit" | sshpass -f "$HPC_SSH_PASSWORD_FILE" ssh dmmo937c@taurus.hrsk.tu-dresden.de
 if [ $? != 0 ]
 then
     echo "Failed to clone repository"
@@ -20,7 +20,7 @@ fi
 echo "SCRATCH_WORKSPACE = $SCRATCH_WORKSPACE"
 echo "SINGULARITY_WORKSPACE_MNT = $SINGULARITY_WORKSPACE_MNT"
 
-sin_app="/root/singularity-executors/singularity_build_project.sh"
+sin_app="/root/scripts/hpc/singularity-executors/singularity_build_project.sh"
 
 app="singularity exec -H ~/:/root --bind $SCRATCH_WORKSPACE:$SINGULARITY_WORKSPACE_MNT:rw  ./ubuntu \"$sin_app\""
 
