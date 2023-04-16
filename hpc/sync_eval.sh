41efa014f8ab67b28a780f9a031c817b6c4dab39
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-16 19:34:42 +0200

    Evaluation job fix (maybe)

--- hpc/sync_eval.sh
+++ hpc/sync_eval.sh
@@ -5,8 +5,15 @@ then
     source "./setvars.sh"
 fi
 
+if [ -z "$CODEBASE_DIR" ]
+then
+    echo "CODEBASE_DIR not set!"
+    exit 1
+fi
+
 results_dir="$BEEGFS_WORKSPACE/omnetpp-results"
-eval_dir="$CODEBASE_DIR/MasterThesis/drones_veins_project/drones_veins_project/simulation/drones_veins_project/eval"
+eval_dir="$CODEBASE_DIR/MasterThesis/stats/eval"
 
 #sshpass -f "$HPC_SSH_PASSWORD_FILE" rsync -r dmmo937c@taurusexport.hrsk.tu-dresden.de:$results_dir ./
+echo "Downloading from $eval_dir to $(pwd)..."
 sshpass -f "$HPC_SSH_PASSWORD_FILE" rsync -r dmmo937c@taurusexport.hrsk.tu-dresden.de:$eval_dir ./
