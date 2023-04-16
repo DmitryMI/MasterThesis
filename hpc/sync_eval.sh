e36c525c3f5292992304cfb3211ab00a4a84705b
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-16 19:22:17 +0200

    sync eval data script added

---
+++ hpc/sync_eval.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$HPC_SETVARS_GUARD" ]
+then
+    source "./setvars.sh"
+fi
+
+results_dir="$BEEGFS_WORKSPACE/omnetpp-results"
+eval_dir="$CODEBASE_DIR/MasterThesis/drones_veins_project/drones_veins_project/simulation/drones_veins_project/eval"
+
+#sshpass -f "$HPC_SSH_PASSWORD_FILE" rsync -r dmmo937c@taurusexport.hrsk.tu-dresden.de:$results_dir ./
+sshpass -f "$HPC_SSH_PASSWORD_FILE" rsync -r dmmo937c@taurusexport.hrsk.tu-dresden.de:$eval_dir ./
