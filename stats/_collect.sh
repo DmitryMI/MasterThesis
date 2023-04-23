41b55bdd2f96ccb7b181187e82f2d5b67a4efe4c
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-23 17:11:32 +0200

    Scavetool variables config file created

--- stats/_collect.sh
+++ stats/_collect.sh
@@ -2,12 +2,14 @@ mkdir -p $EVAL_DIR
 
 FLAGS="-v"
 
+query=$(./get-scavetool-query.py)
+
 input=$(realpath "opp-configs.txt")
 while IFS= read -r opp_config
 do
     echo "Config: $opp_config"
     echo "Exporting to $EVAL_DIR/$opp_config.csv..."
-    scavetool export $FLAGS -o $EVAL_DIR/$opp_config.csv -f "name(totalTimeInJam) OR name(receivedJammingAnnouncementsNum)" $PATH_TO_RAW/$opp_config-*.sca
+    scavetool export $FLAGS -o $EVAL_DIR/$opp_config.csv -f "$query" $PATH_TO_RAW/$opp_config-*.sca
     echo "Done for $opp_config"
     echo ""
     
---
+++ stats/get-scavetool-query.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.11
+
+with open("scavetool-variables.txt", "r") as f:
+    lines = f.readlines()
+    
+fragments = []
+
+for line in lines:
+    fragments.append(f"name({line.rstrip()})")
+    
+query = " OR ".join(fragments)
+
+print(query)
