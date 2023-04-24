581d4b7cc083696115824d9f98bcb2ba779afa33
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-24 22:47:06 +0200

    Executor added

---
+++ hpc/hpc-executors/validate_results.sh
@@ -0,0 +1,24 @@
+cd ~/scripts/hpc/hpc-executors
+pwd
+
+if [ -z "$HPC_SETVARS_GUARD" ]
+then
+    vars=$(realpath "../setvars.sh")
+    if [ ! -f "$vars" ]
+    then
+        echo "$vars does not exist!"
+        exit 1
+    fi
+    source $vars
+fi
+
+if [ -z "$BEEGFS_WORKSPACE" ]
+then
+    echo "BEEGFS_WORKSPACE not defined!"
+    exit 1
+fi
+
+# validator_path="../stats/scave-validator-vs/"
+validator_path="$CODEBASE_DIR/MasterThesis/stats/scave-validator-vs"
+
+$validator_path/ScaveValidator/ScaveValidator.py $validator_path/scave-schema.txt $BEEGFS_WORKSPACE/omnetpp-results -v 1 -o "$BEEGFS_WORKSPACE/ScaveValidator-report.txt "
--- hpc/shell_validate_job.sh
+++ hpc/shell_validate_job.sh
@@ -6,22 +6,27 @@ then
 fi
 
 CPUS=1
-MEM=8192
+MEM=2046
 
-./sync_executors.sh
+# ./sync_executors.sh
+
+if [ $? != 0 ]
+then
+    echo "Failed to upload executor scripts!"
+    exit 1
+fi
 
 echo "cd ./scripts/hpc/hpc-executors/ && source ./clone_repo.sh && exit" | sshpass -f "$HPC_SSH_PASSWORD_FILE" ssh dmmo937c@taurus.hrsk.tu-dresden.de
 if [ $? != 0 ]
 then
-    echo "Failed to clone repository"
+    echo "Failed to clone or pull repository"
     exit 1
 fi
 
 echo "SCRATCH_WORKSPACE = $SCRATCH_WORKSPACE"
 
-sshpass -f "$HPC_SSH_PASSWORD_FILE" scp $PYTHON_FILE_TO_SCHEDULE dmmo937c@taurusexport.hrsk.tu-dresden.de:$SCRATCH_WORKSPACE
+# app="~/scripts/hpc/hpc-executors/validate_results.sh"
+app="$CODEBASE_DIR/MasterThesis/hpc/hpc-executors/validate_results.sh"
 
-app="run-stats.sh -v"
-
-cmd="srun --partition=haswell --ntasks=1 --nodes=1 --cpus-per-task=$CPUS --time=0:30:00 --mem-per-cpu=$MEM \"$app\""
+cmd="srun --partition=haswell --ntasks=1 --nodes=1 --cpus-per-task=$CPUS --time=0:30:00 --mem-per-cpu=$MEM $app"
 echo "cd $CODEBASE_DIR/MasterThesis/stats && $cmd" | sshpass -f "$HPC_SSH_PASSWORD_FILE" ssh -o LogLevel=error dmmo937c@taurus.hrsk.tu-dresden.de
