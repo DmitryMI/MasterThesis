1be01212748268660ee494eebe61fc2602fbf91c
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-04-25 22:35:16 +0200

    Added cd

--- hpc/hpc-executors/hpc_eval_project.sh
+++ hpc/hpc-executors/hpc_eval_project.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
 cd $SCRIPT_DIR
+echo "wd: $(pwd)"
 
-source "./setvars_hpc.sh"
-source "./setvars.sh"
-source "./hpc/setvars.sh"
+source "../../setvars_hpc.sh"
+source "../../setvars.sh"
+source "../setvars.sh"
 
 if [ -z "$CODEBASE_DIR" ]
 then
@@ -15,14 +17,14 @@ else
     echo "CODEBASE_DIR = $CODEBASE_DIR"
 fi
 
-cd $SINGULARITY_WORKSPACE_MNT/MasterThesis/stats/
-source "./setvars.sh"
-
 if [ -z "$BEEGFS_WORKSPACE" ]
 then
     echo "BEEGFS_WORKSPACE not defined!"
     exit 1
 fi
+
+cd $CODEBASE_DIR/MasterThesis/stats/
+source "./setvars.sh"
 
 PATH_TO_RAW="$BEEGFS_WORKSPACE/omnetpp-results"
 source "./run-stats.sh" -c
